5328723362a9ae0472d57c450118fd8bf77fe160
drozd324 <drozdpk@gmail.com>
2025-04-27 21:56:07 +0100

    final commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+Find the report in the report directory.
+Main info in report.pdf
+
+To run all the code necessary compile code in "float" and "double" with a standard
+Makefile with "make" and then you can run "bash runall.sh" in the top directory to 
+run all code neccesary to produce plots in the report. Then to actually make the plots
+int the report run the "task3.py" file in both "double" and "float" and also run 
+"comparison.py" in the "comparison_plots" directory.
--- comparison_plots/compare.py
+++ comparison_plots/compare.py
@@ -20,15 +20,15 @@ matrix_sizes.sort()
 for mat_size in matrix_sizes:
 	plt.clf()
 
-	matrix_df_float = df_float[ df[m] == mat_size ]
+	matrix_df_float = df_float[ df_float[m] == mat_size ]
 	speedup_compute_float = matrix_df_float[speedup_time_compute]
-	matrix_df_double = df_double[ df[m] == mat_size ]
-    speedup_compute_double = matrix_df_double[speedup_time_compute]
+	matrix_df_double = df_double[ df_double[m] == mat_size ]
+	speedup_compute_double = matrix_df_double[speedup_time_compute]
 
 	block = matrix_df_float[block_sizes]
 
-	plt.semilogx(block, speedup_compute, label=f"compute")
-	plt.semilogx(block, speedup_compute, label=f"compute")
+	plt.semilogx(block, speedup_compute_float, label=f"float")
+	plt.semilogx(block, speedup_compute_double, label=f"double")
 
 	plt.xlabel("block size")
 	plt.ylabel("speedup")
@@ -37,27 +37,24 @@ for mat_size in matrix_sizes:
 	plt.savefig(f"./compute_plot_m{mat_size}.png", dpi=300)
 
 
-# comparing reduce functions for avg temp
-#
-#df_old = pd.read_csv("writeup/task3_old_reduce.csv")
-#
-#for mat_size in matrix_sizes:
-#	plt.clf()
-#
-#	matrix_df = df[ df[m] == mat_size ]
-#	speedup_time_averages = matrix_df[speedup_time_calc_averages]
-#
-#	matrix_df_old = df_old[ df_old[m] == mat_size ]
-#	speedup_time_averages_old = matrix_df_old[speedup_time_calc_averages]
-#
-#	block = matrix_df[block_sizes]
-#
-#	plt.semilogx(block, speedup_time_averages, label=f"new")
-#	plt.semilogx(block, speedup_time_averages_old, label=f"old")
-#
-#	plt.xlabel("block size")
-#	plt.ylabel("speedup")
-#	plt.legend()
-#	plt.title(f"Speedup for calculating average m=n={mat_size}")
-#	plt.savefig(f"./writeup/reduce_plot_m{mat_size}.png", dpi=300)
-#
+
+for mat_size in matrix_sizes:
+	plt.clf()
+
+	matrix_df_float = df_float[ df_float[m] == mat_size ]
+	speedup_calc_averages_float = matrix_df_float[speedup_time_calc_averages]
+	matrix_df_double = df_double[ df_double[m] == mat_size ]
+	speedup_calc_averages_double = matrix_df_double[speedup_time_calc_averages]
+
+	block = matrix_df_float[block_sizes]
+
+	plt.semilogx(block, speedup_calc_averages_float, label=f"float")
+	plt.semilogx(block, speedup_calc_averages_double, label=f"double")
+
+	plt.xlabel("block size")
+	plt.ylabel("speedup")
+	plt.legend()
+	plt.title(f"Speedup for calculating averages m=n={mat_size}")
+	plt.savefig(f"./reduce_plot_m{mat_size}.png", dpi=300)
+
+
